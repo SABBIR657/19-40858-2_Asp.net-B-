9f904fdd383c13f753776d0d65824de3dfe65538
SABBIR657 <mdsabbirrahman2025@gmail.com>
2022-09-25 19:55:20 +0600

    first commit

---
+++ CVproject/CVproject/Controllers/CVProjectController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CVproject.Controllers
+{
+    public class CVProjectController : Controller
+    {
+        // GET: CVProject
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Skill()
+        {
+            return View();
+        }
+        public ActionResult Services()
+        {
+            return View();
+        }
+    }
+}
